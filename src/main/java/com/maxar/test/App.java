85513793911a3ca99cc10a8e7eabace78cd60f72
Sofia Prado <sofia.prado.fuentes@gmail.com>
2019-10-31 11:12:30 -0600

    adding spring boot configuration

--- src/main/java/com/maxar/test/App.java
+++ src/main/java/com/maxar/test/App.java
@@ -1,55 +1,16 @@
 package com.maxar.test;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
     Description: Java test application that will be deployed as part of an integration test. It will create a txt file with N
     number of lines every N number of seconds.
     Author: Sofia Prado
  */
+@SpringBootApplication
 public class App 
 {
-    static File file = new File("testFile.txt"); //Set file name
-    static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.S");
-
-    public static void main( String[] args )
-    {
-        String data = "Java integration test file example"; //Text data for the file
-        writeFile(data, 20); //Text data for the file and number of lines to write
-    }
-
-    private static void writeFile(String data, int noOfLines) {
-
-        FileWriter fr = null;
-        BufferedWriter br = null;
-        Date date;
-        try{
-            fr = new FileWriter(file);
-            br = new BufferedWriter(fr);
-            for(int i = noOfLines; i>0; i--){
-                date = new Date(); //Get the current date time
-                String dataWithNewLine=formatter.format(date) + " " + data + System.getProperty("line.separator"); //Text data
-
-                br.write(dataWithNewLine); //write the line
-                TimeUnit.SECONDS.sleep(1); //Wait N seconds until the next record
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally{
-            try {
-                br.close();
-                fr.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
